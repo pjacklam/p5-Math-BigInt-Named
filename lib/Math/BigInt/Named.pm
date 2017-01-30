be4ae4d77f2a3c0051c156ddf9152cb068b9a877
Peter John Acklam <pjacklam@online.no>
2017-01-30 12:46:55 +0100

    import Math-BigInt-Named 0.02 from CPAN
    git-cpan-module: Math-BigInt-Named
    git-cpan-version: 0.02
    git-cpan-authorid: TELS
    git-cpan-file: authors/id/T/TE/TELS/Math-BigInt-Named-0.02.tar.gz

--- lib/Math/BigInt/Named.pm
+++ lib/Math/BigInt/Named.pm
@@ -6,35 +6,59 @@ require 5.005_02;
 use strict;
 
 use Exporter;
-use Math::BigInt(1.47);
+use Math::BigInt::Named;
 use vars qw($VERSION @ISA $PACKAGE @EXPORT_OK
             $accuracy $precision $round_mode $div_scale);
 
 @ISA = qw(Exporter Math::BigInt);
-@EXPORT_OK = qw(bgcd);
 
-$VERSION = 0.01;
+$VERSION = 0.02;
 
 # Globals
 $accuracy = $precision = undef;
 $round_mode = 'even';
 $div_scale = 40;
 
-sub new
-  {
-  my $x = Math::BigInt::new(@_);
-  # Not an integer number? So try if it is a valid name
-  $x = from_name(@_) if $x->is_nan();
-  $x;
-  }
+use Math::BigInt::Named::English;		# default
+
+# Not all of them exist yet
+my $LANGUAGE = {
+  en => 'english',
+  de => 'german',
+  sp => 'spanish',
+  fr => 'french',
+  ro => 'Romana',
+  };
+
+my $LOADED = { };
 
 sub name
   {
   # output the name of the number
-  my $x = shift;
- 
-  return 'one thousand two hundred thirty four' if $x == 1234;
-  return 'one thousand two hundred thirty five';
+  my ($x) = shift;
+
+  return 'NaN' if $x->is_nan();
+
+  my $opt;
+  if (ref($_[0]) eq 'HASH')
+    {
+    $opt = shift;
+    }
+  else
+    {
+    $opt = { @_ };
+    }
+  my $lang = $opt->{language} || 'english';
+  $lang = $LANGUAGE->{$lang} if exists $LANGUAGE->{$lang};	# en => english
+
+  $lang = 'Math::BigInt::Named::' . ucfirst($lang);
+
+  if (!defined $LOADED->{$lang})
+    {
+    eval "use $lang;"; $LOADED->{$lang} = 1;
+    }
+  my $y = $lang->new($x);
+  return $y->name();
   }
 
 sub from_name
@@ -59,7 +83,7 @@ __END__
 
 =head1 NAME
 
-Math::BigInt::Named - Math::BigInt's that can write their own name
+Math::BigInt::Named - Math::BigInts that know their name in some languages
 
 =head1 SYNOPSIS
 
@@ -67,15 +91,16 @@ Math::BigInt::Named - Math::BigInt's that can write their own name
 
   $x = Math::BigInt::Named->new($str);
 
-  print $x->name(),"\n";
+  print $x->name(),"\n";			# default is english
+  print $x->name( language => 'de' ),"\n";	# but German is possible
+  print $x->name( language => 'German' ),"\n";	# like this
+  print $x->name( { language => 'en' } ),"\n";	# this works, too
 
-  print Math::BigInt::Named->from_name('one thousand two hundred fifty),"\n";
+  print Math::BigInt::Named->from_name('einhundert dreiundzwanzig),"\n";
 
 =head1 DESCRIPTION
 
-This is a subclass of Math::BigInt and adds support for named numbers.
-
-Fill in.
+This is a subclass of Math::BigInt and adds support for named numbers. 
 
 =head2 MATH LIBRARY
 
@@ -93,13 +118,6 @@ Math::BigInt::Bar, and when this also fails, revert to Math::BigInt::Calc:
 
 	use Math::BigInt::Named lib => 'Foo,Math::BigInt::Bar';
 
-Calc.pm uses as internal format an array of elements of some decimal base
-(usually 1e7, but this might be differen for some systems) with the least
-significant digit first, while BitVect.pm uses a bit vector of base 2, most
-significant bit first. Other modules might use even different means of
-representing the numbers. See the respective module documentation for further
-details.
-
 =head1 BUGS
 
 None know yet. Please see also L<Math::BigInt>.
@@ -115,11 +133,11 @@ L<Math::BigFloat> and L<Math::Big> as well as L<Math::BigInt::BitVect>,
 L<Math::BigInt::Pari> and  L<Math::BigInt::GMP>.
 
 The package at
-L<http://search.cpan.org/search?mode=module&query=Math%3A%3ABigInt> may
+L<http://search.cpan.org/search?dist=Math-BigInt-Named> may
 contain more documentation and examples as well as testcases.
 
 =head1 AUTHORS
 
-(C) by Tels in late 2001. Based on work by Chris London Noll.
+(C) by Tels in late 2001, early 2002. Based on work by Chris London Noll.
 
 =cut
